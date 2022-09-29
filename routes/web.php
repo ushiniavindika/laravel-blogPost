03f50853bba94a64b8a24519c9c4e7a5d4b32206
ushiniavindika <ushavindika@gmail.com>
2022-09-29 16:52:50 +0530

    My first commit

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->group(function(){
+    Route::get('/dashboard',[App\Http\Controllers\Admin\DashboardController::class,'index']);
+});
